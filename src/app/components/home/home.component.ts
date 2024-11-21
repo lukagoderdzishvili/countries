d42cb772526de8601baab13165ef539af07d879f
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-11-21 14:42:39 +0400

    CHANGE: home component layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  public arr: number[] = new Array(50).fill(0);
 }
